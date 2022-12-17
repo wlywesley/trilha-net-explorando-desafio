6630de3b944d52c56eb197b104341f6311ad65f6
Wesley Sousa <wlywesley123@gmail.com>
2022-12-16 21:06:38 -0300

    Desafio concluído!

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -17,15 +17,19 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (hospedes.Count <= Suite.Capacidade)
             {
                 Hospedes = hospedes;
             }
+
+            /*  NÃO UTILIZADO
             else
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
                 // *IMPLEMENTE AQUI*
-            }
+                throw new Exception("Número de hóspedes maior que a capacidade da suíte.");
+            }  
+            */
         }
 
         public void CadastrarSuite(Suite suite)
@@ -37,7 +41,7 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
             // *IMPLEMENTE AQUI*
-            return 0;
+            return Suite.Capacidade;
         }
 
         public decimal CalcularValorDiaria()
@@ -45,13 +49,14 @@ namespace DesafioProjetoHospedagem.Models
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
             // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            decimal valor = DiasReservados * Suite.ValorDiaria;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                decimal desconto = (valor * 10) / 100;
+                valor = valor - desconto;
             }
 
             return valor;
--- Program.cs
+++ Program.cs
@@ -3,23 +3,88 @@ using DesafioProjetoHospedagem.Models;
 
 Console.OutputEncoding = Encoding.UTF8;
 
-// Cria os modelos de hóspedes e cadastra na lista de hóspedes
+
+// Instância a classe Suite e Reserva.
+Suite suite = new Suite();
+Reserva reserva = new Reserva();
+
+// Escolha da suíte através do menu, onde chama o método da classe Reserva(CadastrarSuite) para salvar as informações.
+Console.WriteLine("Escolha o tipo da suíte. \n");
+
+Console.Write( "1 - Suite Padrão. Capacidade: 2. Valor da diária: 50R$ \n" +  
+               "2 - Suite Média. Capacidade: 5. Valor da diária: 70R$ \n" + 
+               "3 - Suíte Grande. Capacidade: 8. Valor da diária: 90R$ \n");         
+int escolhamenu = Convert.ToInt32(Console.ReadLine());
+
+Console.Clear();
+
+switch(escolhamenu)
+{
+case 1:
+        suite = new Suite(tipoSuite: "Padrão", capacidade: 2, valorDiaria: 50);
+        reserva.CadastrarSuite(suite);
+        break;
+case 2: 
+        suite = new Suite(tipoSuite: "Média", capacidade: 5, valorDiaria: 70);
+        reserva.CadastrarSuite(suite);
+        break;
+case 3:
+        suite = new Suite(tipoSuite: "Padrão", capacidade: 8, valorDiaria: 90);
+        reserva.CadastrarSuite(suite);
+        break;
+default:
+        Console.WriteLine("Opção inválida.");
+        Environment.Exit(0);
+        break;
+}
+
+// Salva a quantidade de dias reservados na propriedade da classe Reserva = DiasReservados.
+Console.Write("Quantos dias pretende reservar? \n");
+reserva.DiasReservados = Convert.ToInt32(Console.ReadLine());
+
+Console.Clear();
+
+// Tratativas de valor e capacidade na quantirdade de hóspedes.
+int hospedesNumero;
+do
+{
+    Console.Write($"Digite o número de hóspedes. (Maximo de {suite.Capacidade} pessoas.) \n");
+    hospedesNumero = Convert.ToInt32(Console.ReadLine());
+
+    if (hospedesNumero > suite.Capacidade)
+    {
+        Console.WriteLine("Capacidade de hóspedes maior que o permitido, favor digitar novamente.");
+    }
+
+    if (hospedesNumero <= 0)
+    {
+        Console.WriteLine("Valor inválido.");
+    }
+}
+while (hospedesNumero > suite.Capacidade || hospedesNumero <= 0);
+
+Console.Clear();
+
+// Pergunta o nome dos hóspedes e salva na lista.
 List<Pessoa> hospedes = new List<Pessoa>();
 
-Pessoa p1 = new Pessoa(nome: "Hóspede 1");
-Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+for (int i = 0; i < hospedesNumero; i++)
+{
+    hospedes.Add(new Pessoa(i.ToString()));
 
-hospedes.Add(p1);
-hospedes.Add(p2);
+    Console.Write($"Digite o nome do {i + 1}° hóspede \n ");
+    hospedes[i].Nome = Console.ReadLine();  
+    Console.Clear(); 
+}
 
-// Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
-
-// Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
-reserva.CadastrarSuite(suite);
 reserva.CadastrarHospedes(hospedes);
 
-// Exibe a quantidade de hóspedes e o valor da diária
-Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
-Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+// Exibe os hóspedes e as informações da reserva.
+int cont = 1;
+foreach (Pessoa pessoa in hospedes)
+{
+        Console.WriteLine($"Hóspede {cont++}: {pessoa.Nome}");
+}
+Console.WriteLine($"Tipo da suíte: {suite.TipoSuite}. Valor da diária: {suite.ValorDiaria.ToString("C")} \n" +
+                  $"Dias reservados: {reserva.DiasReservados} \n" +
+                  $"Valor total das diárias: {reserva.CalcularValorDiaria()}");
